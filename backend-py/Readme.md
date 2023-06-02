4aaf01af74896fe64079e8ad06af9776aad7f995
Andrew Zimmerman <azimmerman@pga.com>
2023-06-02 16:44:50 -0400

    Initalize project with python backend

---
+++ backend-py/Readme.md
@@ -0,0 +1,2 @@
+Run app in developmental server
+flask run --port 8080 --debug
---
+++ backend-py/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+from config import Config
+
+def create_app(config_class=Config):
+  app = Flask(__name__)
+  app.config.from_object(config_class)
+
+  # Initialize Flask extensions here
+
+
+  # migrate models
+
+
+  # Register blueprints here
+
+
+  @app.route('/test')
+  def test_page():
+      return '<h1>Testing the Flask Application Factory Pattern</h1>'
+
+  return app
